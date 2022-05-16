87fe0807d1c993da00fd573f2da9dc6434ed514c
cutten <cutten@latitude-e7450>
2022-05-16 10:15:28 -0400

    Setup project and added images to use later

---
+++ PacPerson/src/pacperson/PacPerson.java
@@ -0,0 +1,27 @@
+/*
+ * B Cutten
+ * May 16, 2022
+ * Tutorial from https://zetcode.com/javagames/pacman/
+ * The goal of the game is to collect all the points in the maze and avoid the ghosts. 
+ * The Pacman is animated in two ways: his position in the maze and his body. We animate 
+ * his body with four images, depending on the direction. The animation is used to create 
+ * the illusion of Pacman opening and closing his mouth. The maze consists of 15x15 squares. 
+ * The structure of the maze is based on a simple array of integers. Pacman has three lives. 
+ * We also count the score.
+ */
+package pacperson;
+
+/**
+ *
+ * @author cutten
+ */
+public class PacPerson {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
